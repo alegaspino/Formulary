e1f9a266ab1e290bc50a865aadd06b71052acfbe
alegaspino <alegaspino@gmail.com>
2016-10-12 22:54:14 -0700

    attempting button display

--- app/src/main/java/com/andrewlegaspino/formulary/FormularyOptionsActivity.java
+++ app/src/main/java/com/andrewlegaspino/formulary/FormularyOptionsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.*;
+import android.widget.LinearLayout.*;
 
 public class FormularyOptionsActivity extends AppCompatActivity {
 
@@ -14,23 +15,34 @@ public class FormularyOptionsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulary_options);
 
-        String[] formularyoptions = getResources().getStringArray(R.array.formularyoptions);
-        LinearLayout linearlayout = (LinearLayout) findViewById(R.id.layout_linear);
+        final String[] formularyoptions = getResources().getStringArray(R.array.formularyoptions);
+        LinearLayout verticalscreen = (LinearLayout) findViewById(R.id.layout_verticalscreen);
 
-        for (String option : formularyoptions) {
-            Button button = new Button(this);
-            button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1f));
-           // need to figure out how to use getResources.getString(<strings.xml resource>)
-           // dont know if getString() takes a string as parameter or R.string.*
-            button.setText(option);
-           // button.setId();
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(FormularyOptionsActivity.this, MathSubjectsActivity.class));
-                }
-            });
-            linearlayout.addView(button);
+        for (int j=0; j<formularyoptions.length; j++) {
+            LinearLayout row = new LinearLayout(this);
+            row.setOrientation(LinearLayout.HORIZONTAL);
+            row.setWeightSum(3.0f);
+            row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 1.0f));
+
+            for (int i = 0; i < 3; i++) {
+                Button button = new Button(this);
+                button.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, 250, 1.0f));
+                button.setText(formularyoptions[j]);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(FormularyOptionsActivity.this, FormulasActivity.class);
+                       // intent.putExtra("BUTTON_OPTION", formularyoptions[j]);
+                        startActivity(intent);
+                    }
+                });
+                row.addView(button);
+                j++;
+                if (j>= formularyoptions.length) { break; }
+            }
+            j--;
+
+            verticalscreen.addView(row);
         }
 
     }
